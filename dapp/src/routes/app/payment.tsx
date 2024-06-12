1a0db1e5d679fb9a8a9fb131c047f64b084700d7
sweetim <hosweetim@gmail.com>
2024-06-12 22:36:21 +0900

    fix color

--- dapp/src/routes/app/payment.tsx
+++ dapp/src/routes/app/payment.tsx
@@ -85,7 +85,7 @@ export default function AppPayment() {
   }
 
   return (
-    <div className="h-full flex flex-col">
+    <div className="h-full flex flex-col bg-[#e2d9ca]">
       <PaymentCard
         name={shop.name}
         amount={amount}
